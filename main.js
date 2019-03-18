a2a8583834d747c1735570b108f826677ecc5fa4
OKOJ <aajmd@hotmail.com>
2019-03-18 16:26:53 -0600

    html file bootstrap firebase

---
+++ main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyArpqt3CnY68BLkmyipB1odJ2TXtyDJVhE",
+    authDomain: "train-scheduler-c261f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c261f.firebaseio.com",
+    projectId: "train-scheduler-c261f",
+    storageBucket: "",
+    messagingSenderId: "927112315163"
+  };
+  firebase.initializeApp(config);
